953cac95b104517d99c14a17265b7a186b6e1211
ambidextrousTx <ravisinha@my.unt.edu>
2016-04-19 19:29:22 -0500

    Add first passing unit test for print statements

---
+++ src/PyBasic.py
@@ -0,0 +1,19 @@
+''' The PyBasic interpreter.
+
+An implementation of BASIC in Python.
+
+Ambidextrous
+Apr 19, 2016
+'''
+
+def execute(command):
+    ''' Executes any single PyBasic command '''
+    if 'PRINT' in command:
+        return execute_print(command)
+
+def execute_print(command):
+    ''' Handles all print statements '''
+    fragments = command.split('"')
+    arguments = fragments[1]
+    arguments_trimmed = arguments.replace('"', '')
+    return arguments_trimmed
---
+++ tests/TestBasicCommands.py
@@ -0,0 +1,18 @@
+''' Tests for basic commands and statements in PyBasic '''
+import unittest
+import sys
+sys.path.append('../src/')
+import PyBasic
+
+class TestBasicCommands(unittest.TestCase):
+    ''' Tests for basic commands and statements in PyBasic '''
+    def test_print_statement(self):
+        ''' Test if the output of PRINT is as expected '''
+
+        command = 'PRINT "HELLO WORLD"'
+        output = PyBasic.execute(command)
+        self.assertEqual(output, 'HELLO WORLD')
+
+
+if __name__ == '__main__':
+    unittest.main()
